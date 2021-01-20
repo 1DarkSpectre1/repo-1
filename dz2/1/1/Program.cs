e3081add9f0e0364b71a0ce4a5663f9b56419f86
1DarkSpectre1 <75720434+1DarkSpectre1@users.noreply.github.com>
2021-01-20 20:09:01 +0400

    Add files via upload

---
+++ dz2/1/1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the minimum daily average temperature");
+            var minT = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the maximum daily average temperature");
+            var maxT = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Average daily temperature today: {(minT + maxT) / 2} ");
+
+            
+        }
+    }
+}
