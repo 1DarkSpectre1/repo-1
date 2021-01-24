aabf0c1718baea1c4824639d037a89cae6ac00aa
1DarkSpectre1 <75720434+1DarkSpectre1@users.noreply.github.com>
2021-01-24 15:00:23 +0400

    Add files via upload

---
+++ dz3/1/1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Введите количество столбцов масива");
+            int cols =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество строк масива");
+            int row = Convert.ToInt32(Console.ReadLine());
+            int[,] array = new int[row, cols];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = j+i;
+                    Console.Write(array[i, j]+" ");
+                }
+                Console.WriteLine();
+            }
+            int minlength;
+                if (array.GetLength(0) < array.GetLength(1))
+                    minlength = array.GetLength(0);
+                else
+                    minlength = array.GetLength(1);
+            for (int i = 0; i < minlength; i++)
+            {
+                Console.WriteLine(array[i, i]);
+            }
+        }
+    }
+}
