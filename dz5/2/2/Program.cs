3f6e2c15fa5f429ea90d694eae345a8eb5508a4d
1DarkSpectre1 <75720434+1DarkSpectre1@users.noreply.github.com>
2021-02-04 18:21:54 +0400

    Add files via upload

---
+++ dz5/2/2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "startup.txt";
+            string str = Convert.ToString(DateTime.Now);
+            File.AppendAllText(filename, str);
+            File.AppendAllText(filename, Environment.NewLine);
+        }
+    }
+}
