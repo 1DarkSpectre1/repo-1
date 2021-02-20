0e1e62e048a84c2885f833799fa9be1a0fb59d18
DarkSpectre <andreu.astrahancev13@mail.ru>
2021-02-20 10:02:26 +0400

    задание 7

---
+++ dz7(Для Андрея)/1/1/FileWt(Для Андрея).cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1
+{
+    class FileWt
+    {
+        public string Title { get; set; }
+
+        public DateTime DataAddFile { get; set; }
+
+        //Title --содержит имя файла (путь)
+        //Дата добавления файла
+
+
+        public List<FileWt> ReadFileAndWriteList(List<FileWt> list)
+        {
+            // Читает данные из файла и проводит сериализацию в класс FileWt и запись в List
+
+            return list;
+        }
+        public List<FileWt> CheckDataFile(List<FileWt> list)
+        {
+            //-- проходит по всему листу и сравнивает дату проверяя дату записи с текущей если разница больше 10 дней запись о файле удаляеется
+            return list;
+        }
+        public bool CheckFile(List<FileWt> list,string filename)
+        {
+            // проверяет обработан ли данный файл
+            return false;
+        }
+
+    }
+}
---
+++ dz7(Для Андрея)/1/1/Program(Для Андрея).cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //При запуске создаём лист и запускаем две функции для проверки уже обработанных файлов
+            //ReadFileAndWriteList()
+            //CheckDataFile()
+            //Далее получаем список всех файлов в директории
+            //проходим по всему списку и для каждого файла выполняем
+            //CheckFile() если файл не обработан WorkToFile() а затем добавление в лист с записью текущей даты
+            //по окончанию работы выполняем WriteFile()
+        }
+        static List<FileWt> WorkToFile(List<FileWt> list)
+        {
+            //-- работа с файлом
+            return list;
+        }
+        static void WriteFile(List<FileWt> list)
+        {
+            //дисериализация и сохранения листа в файл
+        }
+    }
+}
