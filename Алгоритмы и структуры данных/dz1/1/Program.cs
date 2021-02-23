d57c991ece776f703945cf6678a7befa5a1ecb5f
DarkSpectre <andreu.astrahancev13@mail.ru>
2021-02-23 20:13:39 +0400

    Урок 1

---
+++ Алгоритмы и структуры данных/dz1/1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckNumber();
+            //На вход подаем 1, ожидаем Число простое
+            //На вход подаем 4, ожидаем Число не простое
+            //На вход подаем 7, ожидаем Число не простое
+            //На вход подаем отрицательное чилсло, ожидаем Число простое(Согласно блок схеме)
+
+
+        }
+        static void CheckNumber()
+        {
+            int n;
+            Console.WriteLine("Введите число");
+            try
+            {
+                 n = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Ошибка введено некоректное число");
+                return;
+            }
+            int d = 0;
+            int i = 2;
+            while (i<n)
+            {
+                if (n%i==0)
+                {
+                    d++;
+                }
+                    i++;
+            }
+            if (d==0)
+            {
+                Console.WriteLine("Число простое");
+            }
+            else
+            {
+                Console.WriteLine("Число не простое");
+            }
+        }
+    }
+}
---
+++ Алгоритмы и структуры данных/dz1/3/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Число фибоначи = " + Convert.ToString(Fibonachi(number)));
+
+            //На вход подаем 1, ожидаем 1
+            //На вход подаем 2, ожидаем 1
+            //На вход подаем 3, ожидаем 2
+            //На вход подаем 7, ожидаем 13
+            //На вход подаем -1, ожидаем 1
+            //На вход подаем -10, ожидаем 55
+
+        }
+        static int Fibonachi(int number)
+        {
+            if (number == 0)
+            {
+                return number;
+            }
+            if (number == 1)
+            {
+                return number;
+            }
+            if (number == -1)
+            {
+                return -1 * number;
+            }
+            if (number > 0)
+            {
+                return Fibonachi(number - 1) + Fibonachi(number - 2);
+            }
+            else
+            {
+                return Convert.ToInt32(Math.Pow(-1.0, Convert.ToDouble(-1 * number + 1))) * (Fibonachi(-1 * number - 1) + Fibonachi(-1 * number - 2));
+            }
+        }
+    }
+}
