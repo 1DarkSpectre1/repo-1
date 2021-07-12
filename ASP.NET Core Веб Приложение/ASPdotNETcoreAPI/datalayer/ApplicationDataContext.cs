0bdaa679efbbfd9c5fb2c7c0a807e29e0a72129d
DarkSpectre <andreu.astrahancev13@mail.ru>
2021-07-12 19:48:30 +0400

    Урок 2
    добавил изменения

---
+++ ASP.NET Core Веб Приложение/ASPdotNETcoreAPI/datalayer/ApplicationDataContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace datalayer
+{
+    public class ApplicationDataContext : DbContext
+    {
+        public ApplicationDataContext(DbContextOptions<ApplicationDataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Posts> Posts { get; set; }
+    }
+}
